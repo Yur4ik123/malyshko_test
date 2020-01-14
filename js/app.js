8afd09a9ca8567cb7dedae774b085cf9bdc2de47
Yurii_Malyshko <yur.malishko@gmail.com>
2020-01-14 21:05:33 +0200

    add scroll

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@ $(function() {
   $("#speed").niceSelect();
 });
 /* /nice select jQuery */
+
+/* Nicescroll */
+$(".nicescroll").niceScroll({
+  cursorcolor: "#fbd341",
+  cursorwidth: "9px",
+  hidecursordelay: 3000
+});
